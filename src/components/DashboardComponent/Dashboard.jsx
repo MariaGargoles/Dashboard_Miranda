896618cb218c060a94f5aff87ae7429d539717c9
Maria Gargoles <mariagargoles@live.com>
2024-06-26 18:10:15 +0200

    finish KPI styles

--- src/components/DashboardComponent/Dashboard.jsx
+++ src/components/DashboardComponent/Dashboard.jsx
@@ -7,7 +7,7 @@ export const DashboardComponent = () => {
     return (<>
         
             <title>PAGINA</title>
-            <h1>PAGINA CONTENIDO</h1>
+            
             <KPIComponent/>
             <ContactMessagesComponent/>
             </>)
--- src/components/KPIComponent/KPIComponent.jsx
+++ src/components/KPIComponent/KPIComponent.jsx
@@ -1,7 +1,8 @@
 import { IoBedOutline } from "react-icons/io5";
 import { LuCalendarCheck2 } from "react-icons/lu";
 import { IoIosLogIn, IoIosLogOut  } from "react-icons/io";
-import { KPISection, KPICard } from "./KPIStyled";
+import { KPISection, KPICard, KPICardText, KPITitle, KPIText} from "./KPIStyled";
+import "./KPIsComponent.css";
 
 export const KPIComponent = () => {
 
@@ -10,35 +11,35 @@ export const KPIComponent = () => {
         
         <KPISection>
             <KPICard>    
-               <IoBedOutline/>
-                <div>
-                    <h3>8,461</h3>
-                    <p>New Booking</p>
-                </div>
+               <IoBedOutline className="KPIicon"/>
+                <KPICardText>
+                    <KPITitle>8,461</KPITitle>
+                    <KPIText>New Booking</KPIText>
+                </KPICardText>
             </KPICard>
 
            <KPICard>
-               <LuCalendarCheck2/>
-                <div>
-                    <h3>963</h3>
-                    <p>Scheduled Room</p>
-                </div>
+               <LuCalendarCheck2 className="KPIicon"/>
+                <KPICardText>
+                    <KPITitle>963</KPITitle>
+                    <KPIText>Scheduled Room</KPIText>
+                </KPICardText>
             </KPICard>
 
             <KPICard>
-               <IoIosLogIn/>
-                <div>
-                    <h3>753</h3>
-                    <p>Check In</p>
-                </div>
+               <IoIosLogIn className="KPIicon"/>
+                <KPICardText>
+                    <KPITitle>753</KPITitle>
+                    <KPIText>Check In</KPIText>
+                </KPICardText>
             </KPICard>
 
             <KPICard>
-               <IoIosLogOut/>
-                <div>
-                    <h3>516</h3>
-                    <p>Check Out</p>
-                </div>
+               <IoIosLogOut className="KPIicon"/>
+                <KPICardText>
+                    <KPITitle>516</KPITitle>
+                    <KPIText>Check Out</KPIText>
+                </KPICardText>
             </KPICard>
 
         </KPISection>
