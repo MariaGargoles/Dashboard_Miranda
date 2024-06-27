23567c8c6e3b4eb6734e5ec27bfa054a2abab444
Maria Gargoles <mariagargoles@live.com>
2024-06-27 16:13:06 +0200

    create table component for all the tables

---
+++ src/components/TableComponent/TableComponent.jsx
@@ -0,0 +1,30 @@
+import { TableContainer, TableFilters, TableButtonFilter, Table, EncabezadoTabla, BodyTable, TableCell, TableHeadText } from "../ContactComponent/ContactStyled"
+
+export const TableComponent = () => {
+
+    
+    return (
+        <>
+            <Table>
+                <EncabezadoTabla>
+                    <tr>
+                        {columns.map((column, index) => (
+                            <TableHeadText key={index}>{column.headerColumn}</TableHeadText>
+                        ))}
+                    </tr>
+                </EncabezadoTabla>
+                {data.map((row) => (
+                    <BodyTable key={row.id}>
+                        <tr >
+                            {columns.map((col, colIndex) => (
+                                <TableCell key={colIndex}>
+                                    {col.columnRenderer ? col.columnRenderer(row) : row[col.columnsData]}
+                                </TableCell>
+                            ))}
+                        </tr>
+                    </BodyTable>
+                ))}
+            </Table>
+        </>
+    )
+}
