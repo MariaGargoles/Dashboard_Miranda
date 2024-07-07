0ebe2dcf6052e0d39299d1f69e177da39d648989
Maria Gargoles <mariagargoles@live.com>
2024-07-07 16:17:02 +0200

    ADD: dele to users

--- src/components/UsersComponent/UsersComponent.jsx
+++ src/components/UsersComponent/UsersComponent.jsx
@@ -3,9 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addUser, deleteUser, updateUser } from "../../features/Users/UserSlice"; 
 import { TableContainer, TableFilters, TableButtonFilter, Table, EncabezadoTabla, BodyTable, TableCell, TableHeadText, ContactButton } from "../ContactComponent/ContactStyled";
 import { TableComponent } from '../TableComponent/TableComponent';
-import { ImageRoom, StatusButton, SelectorContainer, ButtonRoom, Selector } from "../RoomComponent/RoomStyled";
+import { ImageRoom, StatusButton, SelectorContainer, ButtonRoom, Selector, ActionContainer } from "../RoomComponent/RoomStyled";
 import { UsersButton } from "./UsersStyled";
 import { UsersThunk } from '../../features/Users/UserThunk';
+import { TbEdit, TbTrash } from 'react-icons/tb';
+import Swal from 'sweetalert2';
+
 
 export const UserComponent = () => {
     const dispatch = useDispatch();
@@ -14,6 +17,7 @@ export const UserComponent = () => {
 
     const [currentFilter, setCurrentFilter] = useState('all');
     const [sortOrder, setSortOrder] = useState('asc');
+    const [selectedUser, setSelectedUser] = useState(null);
 
     useEffect(() => {
         if (status === 'idle') {
@@ -47,6 +51,47 @@ export const UserComponent = () => {
         }
     });
 
+    const handleEditUser = (user) => {
+        setSelectedUser(user);  // Set the user to be edited
+    };
+
+    const handleDeleteUser = (userId) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                dispatch(deleteUser(userId))
+                    .then(() => {
+                        Swal.fire('Deleted!', 'The user has been deleted.', 'success');
+                    })
+                    .catch((error) => {
+                        Swal.fire('Error!', 'There was an error deleting the user.', 'error');
+                    });
+            }
+        });
+    };
+
+    const handleSaveUser = (user) => {
+        dispatch(updateUser(user))
+            .then(() => {
+                Swal.fire('Updated!', 'The user has been updated.', 'success');
+                setSelectedUser(null);  
+            })
+            .catch((error) => {
+                Swal.fire('Error!', 'There was an error updating the user.', 'error');
+            });
+    };
+
+    const handleCloseModal = () => {
+        setSelectedUser(null);
+    };
+
     const columns = [
         { headerColumn: 'Photo', columnsData: 'foto', columnRenderer: (row) => <img src={row.foto} alt="Employee" style={{ width: '50px', borderRadius: '50%' }} /> },
         { headerColumn: 'Name', columnsData: 'name' },
@@ -56,6 +101,15 @@ export const UserComponent = () => {
         { headerColumn: 'Email', columnsData: 'email' },
         { headerColumn: 'Contact', columnsData: 'contact' },
         { headerColumn: 'Status', columnsData: 'status', columnRenderer: (row) => <UsersButton status={row.status}>{row.status}</UsersButton> },
+        {
+            headerColumn: 'Actions',
+            columnRenderer: (row) => (
+                <ActionContainer>
+                    <TbEdit title="Edit User" onClick={() => handleEditUser(row)} />
+                    <TbTrash title="Delete User" onClick={() => handleDeleteUser(row.id)} />
+                </ActionContainer>
+            )
+        },
     ];
 
     return (
@@ -75,6 +129,13 @@ export const UserComponent = () => {
                 </SelectorContainer>
                 <TableComponent columns={columns} data={sortedData} />
             </TableContainer>
+            {selectedUser && (
+                <EditUserModal
+                    user={selectedUser}
+                    onSave={handleSaveUser}
+                    onCancel={handleCloseModal}
+                />
+            )}
         </>
     );
 };
