328f87bd560f9d1eaea31f260e8f83cda65150f7
MariaGargoles <mariagargoles@live.com>
2024-07-20 15:26:57 +0200

    FINISH TS PROYECT

--- src/pages/Booking/NewBookinRoom.tsx
+++ src/pages/Booking/NewBookinRoom.tsx
@@ -1,4 +1,4 @@
-import { NewBookin } from "../../components/BookingComponent/NewBookinComponent"
+import { NewBookin } from "../../components/BookingComponent/NewbookinComponent"
 import { NavbarComponent } from "../../components/NavbarComponent/NavbarComponent"
 
 
