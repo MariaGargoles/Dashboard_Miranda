c8d04132852f7f38253397fbe667c620792ee9a8
MariaGargoles <mariagargoles@live.com>
2024-07-20 14:10:32 +0200

    ADD: add form login

--- src/components/FormLoginComponent/FormLoginComponent.tsx
+++ src/components/FormLoginComponent/FormLoginComponent.tsx
@@ -1,25 +1,38 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { loginUser } from '../../context/AuthUserContext.jsx';
+import { login } from '../../context/AuthUserContext';
 import "./FormLoginComponent.css";
 import { LoginForm, TitleForm, Form, Label, Input, LogoForm } from "./FormLoginStyled.js";
 import logo from "../../assets/Logo.png"
 
-export const FormLoginComponent = () => {
+export const FormLoginComponent: React.FC = () => {
   const dispatch = useDispatch();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const submitHandler = (event) => {
+
+  interface AuthState {
+    foto: string;          
+    name: string;
+    id: string;
+    startDate: string;
+    description: string;
+    email: string;
+    contact: string;
+    status: string;
+    isAuthenticated: boolean;
+  }
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (username === "maria" && password === "miranda") {
-      dispatch(loginUser({ name: username, email: "maria@example.com" }));
+      dispatch(login({ name: username, email: "maria@example.com" }));
     } else {
       alert("Datos incorrectos");
     }
   };
-
+  
   return (
     <LoginForm>
       <TitleForm>LoginPage</TitleForm>
